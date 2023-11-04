423266a2fb12b7bce9d01211c9278278acb668bb
abhibreddy <88990465+abhibreddy@users.noreply.github.com>
2023-11-04 18:16:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var madlib = 'Once upon a time, there was a brave (noun) named (name). He was always ready for an (adjective) adventure. One day, he decided to explore a mysterious (noun) deep in the (noun). As he ventured further, he heard a (adjective) noise coming from the (noun). He cautiously stepped forward, and to his surprise, he found a (adjective) dragon! The dragon had (adjective) eyes and a (adjective) tail. Together, they traveled the (noun) and had many (adjective) adventures.';
+
+console.log(madlib.split(/\(noun\)|\(name\)|\(adjective\)/));
